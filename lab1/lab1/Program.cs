972e919b75a319dca7f9c0b18914f3b4b63c8ca4
Mariaxuf22 <97722846+Mariaxuf22@users.noreply.github.com>
2023-05-10 11:00:51 +0300

    Add files via upload

---
+++ lab1/lab1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 2; 
+            double b = 4; 
+
+            double logab = Math.Log(b, a);
+            double ln_a = Math.Log(a);
+            double ln_b = Math.Log(b);
+
+            double lhs = logab;
+            double rhs = ln_a / ln_b;
+
+            double epsilon = 0.000001; 
+            bool areEqual = Math.Abs(lhs - rhs) < epsilon;
+
+            Console.WriteLine($"log({a},{b}) = {logab}");
+            Console.WriteLine($"ln({a}) / ln({b}) = {rhs}");
+            Console.WriteLine($"Are they equal? {areEqual}");
+            
+        }
+    }
+}
